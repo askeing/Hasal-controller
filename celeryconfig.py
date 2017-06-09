163d8b6809b7a6ffa6a9880414dec5e9bc242be8
Askeing <askeing@gmail.com>
2017-06-09 11:40:46 +0800

    Backward Compatible of CeleryConfig
    Ref: http://docs.celeryproject.org/en/latest/userguide/configuration.html#new-lowercase-settings

--- celeryconfig.py
+++ celeryconfig.py
@@ -5,5 +5,16 @@ result_backend = 'rpc://'
 task_serializer = 'json'
 result_serializer = 'json'
 accept_content = ['json', 'pickle']
-# timezone = 'Asia/Taipei'
+timezone = 'Asia/Taipei'
 enable_utc = True
+
+# Backward Compatible
+# Ref: http://docs.celeryproject.org/en/latest/userguide/configuration.html#new-lowercase-settings
+BROKER_URL = broker_url
+CELERY_RESULT_BACKEND = result_backend
+
+CELERY_TASK_SERIALIZER = task_serializer
+CELERY_RESULT_SERIALIZER = result_serializer
+CELERY_ACCEPT_CONTENT= accept_content
+CELERY_TIMEZONE = timezone
+CELERY_ENABLE_UTC = enable_utc
